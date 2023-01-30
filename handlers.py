8e4e4fab331ab5671edbd1317b2642153a4aaa28
Aleksei Riabinin <aleksei.riabinin@yahoo.com>
2023-01-30 19:44:44 +0300

    added comments

--- handlers.py
+++ handlers.py
@@ -1,8 +1,9 @@
 from loader import dp, bot
 from aiogram import types
+from random import randint
 
 total = 150
-turn = 'Human'
+turn = ''
 
 @dp.message_handler(commands=['start'])
 async def mes_start(message: types.Message):
@@ -31,8 +32,10 @@ async def mes_human(message: types.Message):
 
     if candies < 28:
         total -= candies
-        turn = 'Human'
+        turn = 'Robot'
         await bot.send_message(message.from_user.id, f'{total} candies are left on the table')
+        if total < 29:
+            await bot.send_message(message.from_user.id, f'{turn} wins!')    
     else:
         await bot.send_message(message.from_user.id, f'Enter a number less than 28')
                
@@ -40,46 +43,24 @@ async def mes_human(message: types.Message):
 async def mes_robot(message: types.Message):
     global total
     global turn
+
+    turn = 'Human'
     await bot.send_message(message.from_user.id, f"Robot's turn")
-    
-    count = message.text.split()[1]
-    candies = int(count)
 
-    if candies < 28:
+    if total > 57:
+        candies = randint(1, 29)
         total -= candies
-        turn = 'Robot'
-        await bot.send_message(message.from_user.id, f'{total} candies are left on the table')
-    else:
-        await bot.send_message(message.from_user.id, f'Enter a number less than 28')
-
-# @dp.message_handler()
-# async def mes_all(message: types.Message):
-#     global total
-#     global turn
     
-#     if message.text.isdigit() and int(message.text) < 28:
-#         total -= int(message.text)
-#         await bot.send_message(message.from_user.id, f'{total} candies are left on the table')
-#     else:
-#         await bot.send_message(message.from_user.id, f'Enter a number')        
-
-# @dp.message_handler()
-# async def mes_all(message: types.Message):
-#     global total
-#     global turn
+    elif 28 < total < 57:    
+        candies = 1
+        total -= candies
     
-#     if message.text.isdigit():
-#         total -= int(message.text)
-#         await bot.send_message(message.from_user.id, f'{total} candies are left on the table')
-#     else:
-#         await bot.send_message(message.from_user.id, f'Enter a number')
-
-# @dp.message_handler(commands=['OOP'])
-# async def mes_start(message: types.Message):
-#     await message.answer(f'Oh, it is really cool') 
-
-# @dp.message_handler(commands=['toxic'])
-# async def mes_toxic(message: types.Message):
-#     await bot.delete.message(message.from_user.id, message.message_id)
-#     await message.answer(f'This is a toxic word')
+    else:    
+        candies = total
+        total -= candies
+        
+    if total < 29:
+        await bot.send_message(message.from_user.id, f'{turn} wins!')
+    
+    await bot.send_message(message.from_user.id, f'{total} candies are left on the table')
                     
