82160b6de6b9a6c1fdedb4eb8eacec8ed954ce8d
Aleksei Riabinin <aleksei.riabinin@yahoo.com>
2023-02-02 02:06:45 +0300

    added new files

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from handlers import dp
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import executor
 
 async def on_start(_):
     print('Bot is running')
+    # await dp.bot.send_message(message.from_user.id, f'Chat is joined by {message.from_user.full_name}')
 
-executor.start_polling(dp, skip_updates=True, on_startup=on_start)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True, on_startup=on_start)
